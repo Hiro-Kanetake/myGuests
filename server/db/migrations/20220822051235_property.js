542ac5efafc46b9483ea15af6d82eebc2713e8c3
xxxx <git@github.com>
2022-08-22 14:35:25 +0900

    feat: Create migration property

---
+++ server/db/migrations/20220822051235_property.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('property', function (table) {
+        table.increments('id');
+        table.string('name', 255).notNullable();
+        table.integer('pincode').notNullable();
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema
+    .dropTable("property");
+};
