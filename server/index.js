d00227fb89d1169a512230e38381f574599130bd
xxxx <git@github.com>
2022-08-22 12:16:18 +0900

    initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 8080;
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to myGuests Server!!');
+});
+
+app.listen(PORT, () => {
+  console.log(`myGuests server listening at http://localhost:${PORT}`);
+});
