d38e1b79c33f36d77f7199d1ddde85b36a2e3d36
xxxx <git@github.com>
2022-08-22 14:35:49 +0900

    feat: Create migration account

---
+++ server/db/migrations/20220822052157_account.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('account', function (table) {
+        table.increments('id');
+        table.string('first_name', 255).notNullable();
+        table.string('last_name', 255).notNullable();
+        table.string('position', 255).notNullable();
+        table.string('username', 255).notNullable();
+        table.integer('property_id').notNullable().references("id").inTable("property").onDelete("CASCADE");
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable("account");
+};
