cbe0e0f5ed9d6e8ae91692a3753bd1fac8211908
xxxx <git@github.com>
2022-08-22 14:31:46 +0900

    style: Add white space

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
