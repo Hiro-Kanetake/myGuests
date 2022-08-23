56b3f917cec64da5dd494cf3a53fa1518a47e97e
xxxx <git@github.com>
2022-08-23 13:22:55 +0900

    feat: Set react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import './App.css';
+import TopPage from './TopPage'
+import PropertyMainPage from './PropertyMainPage'
+import AddGuest from './AddGuest'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TopPage />} />
+          <Route path="/property" element={<PropertyMainPage />} />
+          <Route path="/addGuest" element={<AddGuest />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
